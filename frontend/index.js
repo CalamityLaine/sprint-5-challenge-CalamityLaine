fd653acad3f30d84a8f69827f057f34fa102cba7
Laine Flanagan <lainerflanagan@gmail.com>
2024-03-21 17:34:31 -0500

    Everything but mentors

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-//async function sprintChallenge5( ) { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
+async function sprintChallenge5( ) { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
   const footer = document.querySelector('footer')
@@ -57,30 +57,62 @@ async function fetchAndCombineData() {
 }
 
   function buildLearnerCard(learner) {
-    const card = document.createElement('div');
+    let card = document.createElement('div');
     card.classList.add('card');
+  //  card.textContent = (learnerName, learnerEmail, learnerMentors);
+
+ // const learnerName = document.querySelector('h3');
+ // learnerName.textContent = `${learner.fullName}`
+
+//  const learnerEmail = document.querySelector('div');
+//  learnerEmail.textContent = `${learner.email}`
+
+// const learnerMentors = document.querySelector('h4');
+// learnerMentors.textContent = "▶ Mentors";
+
+//  card.appendChild('learnerName');
+//  card.appendChild('learnerEmail');
+//  card.appendChild('learnerMentors');
+
+
+
     card.innerHTML = `
-      <h3>${learner.fullName}</h3>
-      <div>${learner.email}</div>
-      <h4>
+     <h3>${learner.fullName}</h3>
+     <div>${learner.email}</div>
+     <h4>▶ Mentors
       <class="closed"</>
-      </h4>
-      <ul> ${learner.mentors.map(mentor => `<li>${mentor}</li>`).join('')}</ul>
+     </h4>
     `;
   
   
     // Add click event listener to the card
-    card.addEventListener('click', event => {
-      card.classList.toggle('selected')
-      const target = event.target;
+    card.addEventListener('click', evt => {
+      const cardSelected = card.classList.contains('selected');
+
+      document.querySelectorAll('.card').forEach(card => {
+        card.classList.remove('selected')
+        info.textContent = "No learner is selected"
+      });
+      
+     // const target = evt.target;
   
       // Check if the click target is the toggle button
-      if (target.classList.contains('selected')) {
+      if (!cardSelected) {
+        card.classList.add('selected');
         info.textContent = `The selected learner is ${learner.fullName}`
-        const ul = card.querySelector('ul');
-        ul.classList.toggle('closed');
+       
+        card.innerHTML = `
+     <h3>${learner.fullName}, ID ${learner.id}</h3>
+     <div>${learner.email}</div>
+     <h4>▼ Mentors
+      <class="open"</>
+     </h4>
+     <ul> ${learner.mentors.map(mentor => `<li>${mentor}</li>`).join('')}</ul>
+    `;
+    //const ul = card.querySelector('ul');
+    //ul.classList.toggle('selected');
         
-      }
+   }
       
     });
   
@@ -107,12 +139,11 @@ async function fetchAndCombineData() {
   renderLearnerCards().catch(error => {
     console.error('Error rendering learner cards:', error);
   });
-
+}
   //    console.log('😔 Promise rejected with an err.message -->', err.message)
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 
  //❗ DO NOT CHANGE THE CODE  BELOW
-//if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
-//else sprintChallenge5()
-//}
+if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
+else sprintChallenge5()
